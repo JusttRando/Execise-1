93b5bab4de28a0f82101e4756f297d5326dd89c6
Rando <justtrando@gmail.com>
2025-10-28 14:49:53 +0200

    Start learning javascript

---
+++ Execise_1_Loyiso_Hans.js
@@ -0,0 +1,23 @@
+// TODO: Create a variable called firstName and store your first name in it
+
+const firstName = "Loyiso";
+
+// TODO: Create a variable called lastName and store your last name in it
+
+const lastName = "Hans";
+
+// TODO: Create a variable called age using let (you'll modify this later)
+
+let age;
+
+// TODO: Create a fullName variable that combines firstName and lastName
+
+const fullName = `${firstName} ${lastName}`;
+
+// TODO: Print a greeting to the console using these variables
+
+console.log(`Hello, me nam ${fullName} I am ${age} years old maybe.`);
+
+// TODO: Try changing the age variable to a different value
+
+age = 18;
